52ca5c2d3f4e639ae8538ba55fe799a9f022aa66
Arthur Araruna <ararunaufc@gmail.com>
2018-09-27 01:00:54 -0300

    Alterado visual de ponto destacado. Adicionado *antialiasing* no desenho dos pontos.
    Signed-off-by: Arthur Araruna <ararunaufc@gmail.com>

--- Pontos/mainwindowcontroller.cpp
+++ Pontos/mainwindowcontroller.cpp
@@ -2,10 +2,11 @@
 #include "ui_mainwindow.h"
 
 #include <QShortcut>
-#include <QGraphicsItem>
+#include <QGraphicsEllipseItem>
 
 const QPen   MainWindowController::pointNoContour{Qt::NoPen};
-const QPen   MainWindowController::pointContour{Qt::black, 2};
+const QPen   MainWindowController::pointContour{Qt::black, 3};
+const QBrush MainWindowController::pointNoFill{Qt::NoBrush};
 const QBrush MainWindowController::pointFill{Qt::red};
 const qreal  MainWindowController::pointWidth{10};
 
@@ -13,13 +14,35 @@ MainWindowController::MainWindowController(MainWindow &mainWindow, QObject *pare
     : QObject{parent}
     , mainWindow{mainWindow}
 {
-    mainWindow.ui->graphicsView->setScene(&scene);
+    auto ui = mainWindow.ui;
 
-//    scene.addEllipse(-15/2.0, -15/2.0, 15, 15, Qt::NoPen, Qt::red)->setFlag(QGraphicsItem::ItemIgnoresTransformations, true);
+    ui->graphicsView->setScene(&scene);
+    ui->graphicsView->setRenderHint(QPainter::Antialiasing, true);
 
-//    mainWindow.ui->graphicsView->scale(3,3);
+}
 
-//    auto item = scene.itemAt(0,0,mainWindow.ui->graphicsView->transform());
-//    if(item) item->setOpacity(0.5);
+std::vector<QPointF> MainWindowController::getAllPoints() const
+{
+    const auto allItems = scene.items();
+    std::vector<QPointF> allPoints;
 
+    allPoints.reserve(static_cast<size_t>(allItems.size()));
+
+    for(QGraphicsItem *item : allItems) {
+        allPoints.push_back(item->pos());
+    }
+
+    return allPoints;
+}
+
+void MainWindowController::highlightPoint(const QPointF &point)
+{
+    QGraphicsEllipseItem *item = qgraphicsitem_cast<QGraphicsEllipseItem*>(
+                scene.itemAt(point, mainWindow.ui->graphicsView->transform())
+                );
+
+    if(item) {
+        item->setPen(MainWindowController::pointContour);
+        item->setBrush(MainWindowController::pointNoFill);
+    }
 }
--- Pontos/mainwindowcontroller.hpp
+++ Pontos/mainwindowcontroller.hpp
@@ -8,14 +8,21 @@
 #include <QBrush>
 #include <QPen>
 
+#include <vector>
+
 class MainWindowController final : public QObject
 {
     Q_OBJECT
 public:
     explicit MainWindowController(MainWindow &mainWindow, QObject *parent = nullptr);
 
+    std::vector<QPointF> getAllPoints() const;
+
+    void highlightPoint(QPointF const &point);
+
     static const QPen   pointNoContour;
     static const QPen   pointContour;
+    static const QBrush pointNoFill;
     static const QBrush pointFill;
     static const qreal  pointWidth;
 
